5db8d66f84855315b1f51d84f9fc166b92a43748
Inas Najah Z <134346474+inassiperboi@users.noreply.github.com>
2024-12-08 17:09:12 +0700

    Delete UI directory

--- UI/android/app/src/main/kotlin/com/example/tokodia/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.tokodia
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- UI/lib/base_url.dart
+++
@@ -1,4 +0,0 @@
-// Masukan IP disini
-const String ip = '192.168.0.3';
-
-const String baseUrl = 'http://$ip:8080/';
